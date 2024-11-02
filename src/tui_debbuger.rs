74baabfcc1344954a8694793b79f9d35300be358
myuon <ioi.joi.koi.loi@gmail.com>
2024-11-02 23:08:07 +0900

    feat: debugger app widget

--- src/tui_debbuger.rs
+++ src/tui_debbuger.rs
@@ -1,30 +1,94 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
 use anyhow::Result;
-use crossterm::event::{self, KeyEventKind};
-use ratatui::{style::Stylize, widgets::Paragraph, DefaultTerminal};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::Line,
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+use crate::compiler::runtime::Runtime;
 
 pub fn start_tui_debugger() -> Result<()> {
     let mut terminal = ratatui::init();
     terminal.clear()?;
 
-    let app_result = run(terminal);
+    let mut debugger = Debugger {
+        runtime: Arc::new(Mutex::new(Runtime::new(1024, vec![], HashMap::new()))),
+        exit: false,
+    };
+    let app_result = debugger.run(&mut terminal);
     ratatui::restore();
 
     app_result
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello TUI Debugger! (press 'q' to quit)")
-                .white()
-                .on_dark_gray();
-            frame.render_widget(greeting, frame.area());
-        })?;
+struct Debugger {
+    runtime: Arc<Mutex<Runtime>>,
+    exit: bool,
+}
 
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == event::KeyCode::Char('q') {
-                return Ok(());
-            }
+impl Debugger {
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_event();
         }
+
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_event(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event);
+            }
+            _ => (),
+        };
+
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit = true,
+            _ => {}
+        }
+    }
+}
+
+impl Widget for &Debugger {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" TUI Debugger ").bold();
+        let instructions = Line::from(vec![
+            " Resume ".into(),
+            "<R>".blue().into(),
+            " Step ".into(),
+            "<S>".blue().into(),
+            " Quit ".into(),
+            "<Q> ".blue().into(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions)
+            .border_set(border::THICK);
+
+        Paragraph::new("foobar").block(block).render(area, buf);
     }
 }
